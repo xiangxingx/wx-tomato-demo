aa0fda3460ffc44a17670a5198538dfc8356d605
xiangxingx <47742377+xiangxingx@users.noreply.github.com>
2019-08-03 10:03:56 +0800

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-App({
-  globalData: {
-    host: 'https://gp-server.hunger-valley.com',
-    app_id: "wx1d06978e87d9012f",
-    app_secret: "ed2467467486716973cf44530f83526f",
-    t_app_id: "9XZHqSkjVjaEme7Z47MaDpdj",
-    t_app_secret: "F4s16T8nX8hznomXm3P5utZV"
-  }
-})
